2ed8f43a9afbb1eb57680adcec42249066c1d72f
Xhevdet20 <34029916+Xhevdet20@users.noreply.github.com>
2019-09-18 16:25:09 +0200

    Add files via upload

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,14 +1,16 @@
-const key = 'um4AS0ytmoXifd0qtllV7wvfyKvoeO5m';
+const key = '9RlzxbzEcjPeAEVMvQrpWIjM2H26twUB';
+
+
 
 // Get current weather information
 const getWeather = async (id) => {
 
   const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-  const query = `${id}?apikey=${key}`;
+  const query = `${id}?apikey=${'9RlzxbzEcjPeAEVMvQrpWIjM2H26twUB'}`;
 
   const response = await fetch(base + query);
   const data = await response.json();
-
+  
   return data[0];
 };
 
@@ -16,7 +18,7 @@ const getWeather = async (id) => {
 
 const getTomorrowWeather = async(id) => {
   const base = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/';
-  const query = `${id}?apikey=${key}`;
+  const query = `${id}?apikey=${'9RlzxbzEcjPeAEVMvQrpWIjM2H26twUB'}`;
 
   const response = await fetch(base + query);
   const data = await response.json();
@@ -32,7 +34,7 @@ const getTomorrowWeather = async(id) => {
 const getCity = async (city) => {
 
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-  const query =`?apikey=${key}&q=${city}`;
+  const query =`?apikey=${'9RlzxbzEcjPeAEVMvQrpWIjM2H26twUB'}&q=${city}`;
 
   const response = await fetch(base + query);
   const data = await response.json();
